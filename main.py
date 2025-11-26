a285b7f80ab768136616b0fa45a6db39909a3a82
Paradise1502 <felix_meyer15@outlook.de>
2025-11-26 09:01:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ SHEET_NAME = "Event Schedule"
 SHEET_HEADERS = ["event_name","start_time_utc","channel_id","message","event_type","ping_role_id"]
 
 # ---------- Roles ----------
-DEFAULT_ROLE_ID = 1430370436222550046  # test role; change later if needed
+DEFAULT_ROLE_ID = 1235729244605120572  # test role; change later if needed
 
 # ---------- Reminder schedules ----------
 REMINDERS = {
